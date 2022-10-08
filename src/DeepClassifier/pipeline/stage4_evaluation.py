b4f644883bcce11f40b8e9d245e54f0894d395fe
Shubhamlad1 <shubhamlad33@gmail.com>
2022-10-08 17:45:42 +0530

    Evaluation Stage

---
+++ src/DeepClassifier/pipeline/stage4_evaluation.py
@@ -0,0 +1,27 @@
+from DeepClassifier import logger
+from DeepClassifier.config.configuration import ConfigurationManager
+from DeepClassifier.components import Evaluation
+from DeepClassifier.entity import EvaluationConfig
+
+STAGE_NAME= "Model Evaluation"
+
+def main():  
+    config= ConfigurationManager()
+
+    #----object creation for class Configmanager--------
+
+    config = ConfigurationManager()
+    val_config = config.get_evaluation_config()
+    evaluation = Evaluation(EvaluationConfig)
+    evaluation.evaluation()
+    evaluation.save_scores()
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>>>>> Stage Name: {STAGE_NAME} Started")
+        main()
+        logger.info(f">>>>>>>>> Stage Name: {STAGE_NAME} Completed/n")
+        logger.info("---------------------------------------------------------\n")
+    except Exception as e:
+        logger.info(e)
+        raise e
